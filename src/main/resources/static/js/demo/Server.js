411d279b8c37368ef0d85a9692acaf3a141dc545
Ahluo <52STE@sina.com>
2016-09-20 01:00:15 +0800

    add demo server

---
+++ src/main/resources/static/js/demo/Server.js
@@ -0,0 +1,53 @@
+let http = require('http');
+let url = require('url');
+let path = require('path');
+let fs = require('fs');
+
+let mine = require('./mine.js');
+
+const PORT = 8080;
+const IP = '192.168.1.163';
+const assets = 'html';
+
+function handleRequest(request,response){
+
+   let pathName = url.parse(request.url).pathname;
+   let realPath = assets + pathName;
+   let extType = path.extname(realPath);
+   extType = extType ? extType.slice(1) : 'unknown';
+   console.log('current ext type : ' +extType);
+   fs.stat(realPath , function(error,stats){
+        //undefined stats ; WARNNING:https://nodejs.org/api/fs.html#fs_fs_stat_path_callback
+        console.log('Stats Parameter: ' + stats);
+        if(error){
+            response.write("This Request URL " + pathName + " was not found on this server.");
+            response.end();
+        }else{
+            fs.readFile(realPath,"binary",function(error,file){
+                if(error){
+                    response.writeHead(500,{'Content-Type':'text/plain'});
+                    response.end(error);
+                }else{
+                    let contentType = mine[extType];
+                    console.log('current file type: ' + contentType);
+                    response.writeHead(200,{'Content-Type':contentType});
+                    response.write(file,"binary");
+                    response.end();
+                }
+            });
+        }
+   });
+
+    request.on('data',function(chunk){
+        console.log("Request Body: " + chunk);
+    });
+
+
+//    response.end("It works ! Path Hit: " + request.url + '\n');
+}
+
+let server = http.createServer(handleRequest);
+
+server.listen(PORT,IP,function(){
+    console.log(`Server Started....URL:http://${IP}:${PORT}`);
+});
--- src/main/resources/static/js/web/HelloWebServer.js
+++ src/main/resources/static/js/web/HelloWebServer.js
@@ -1,9 +1,18 @@
 var http = require('http');
 
-http.createServer(function(request,response){
+let server = http.createServer(function(request,response){
+
+    console.log('Request Method: ' + request.method);
+
+    request.on('data',function(chunk){
+        console.log('Chunk : ' + chunk);
+    });
+
     response.setHeader('Ahluo','DemoClient');
     response.writeHead(200,{'Content-Type':'text-plain'});
     response.end('Hello Web\n');
-}).listen(8080);
+});
+
+server.listen(8080);
 
 console.log('Server Started...');
